59e64ec6d51173d4f472c556300e12a849cbceb9
korundM3 <szyt0207@gmail.com>
2026-03-13 00:37:05 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
+    localStoragePrefix: "98so",
+    title: "98so",
     description: "Party rank sorter for your custom list of songs."
 };
